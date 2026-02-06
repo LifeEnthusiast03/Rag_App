0787b7fb0eecf76a2ca4899aee80fea39d3f407a
SOUGATA SAHA <sahasougata820@gmail.com>
2026-02-06 22:22:31 +0530

    remove backend from this folder

--- backend/pymodel.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-class chat_his(BaseModel):
-    role:str
-    content:str
-class ChatRequest(BaseModel):
-    chat_id: str
-    question: str
-    chat_history: List[chat_his]
